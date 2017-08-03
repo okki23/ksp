e0dda9ef5b51f401380f821e72bc5b372eb78d8d
okki23 <okkisetyawan@gmail.com>
2017-08-04 00:00:12 +0700

    update db

--- db_ksp.sql
+++ db_ksp.sql
@@ -1,125 +1,90 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 03 Agu 2017 pada 18.15
--- Versi Server: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : localhost_mysql
+Source Server Version : 50616
+Source Host           : 127.0.0.1:3306
+Source Database       : db_ksp
 
+Target Server Type    : MYSQL
+Target Server Version : 50616
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+Date: 2017-08-03 23:59:55
+*/
 
---
--- Database: `db_ksp`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tb_anggota`
---
-
+-- ----------------------------
+-- Table structure for tb_anggota
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_anggota`;
 CREATE TABLE `tb_anggota` (
   `kode_anggota` varchar(20) NOT NULL,
   `nama_anggota` varchar(100) NOT NULL,
   `jenis_kelamin` enum('Laki-Laki','Perempuan','','') NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `No_Telp` varchar(20) NOT NULL,
-  `gambar` varchar(50) NOT NULL
+  `gambar` varchar(50) NOT NULL,
+  PRIMARY KEY (`kode_anggota`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_anggota`
---
+-- ----------------------------
+-- Records of tb_anggota
+-- ----------------------------
+INSERT INTO `tb_anggota` VALUES ('112233', 'Nindyary', 'Perempuan', 'Jakarta', '08383180xxxx', '');
+INSERT INTO `tb_anggota` VALUES ('12345', 'Romy', 'Laki-Laki', 'Bekasi', '08965056xxxx', '');
 
-INSERT INTO `tb_anggota` (`kode_anggota`, `nama_anggota`, `jenis_kelamin`, `alamat`, `No_Telp`, `gambar`) VALUES
-('112233', 'Nindyary', 'Perempuan', 'Jakarta', '08383180xxxx', ''),
-('12345', 'Romy', 'Laki-Laki', 'Bekasi', '08965056xxxx', '');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tb_login`
---
-
+-- ----------------------------
+-- Table structure for tb_login
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_login`;
 CREATE TABLE `tb_login` (
-  `kode_user` int(11) NOT NULL,
+  `kode_user` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(40) NOT NULL,
   `password` varchar(40) NOT NULL,
   `nama_lengkap` varchar(100) NOT NULL,
-  `level` enum('admin','operator','','') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `level` enum('admin','operator','','') NOT NULL,
+  PRIMARY KEY (`kode_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_login`
---
+-- ----------------------------
+-- Records of tb_login
+-- ----------------------------
+INSERT INTO `tb_login` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Febriansyah Tri Romdhoni', 'admin');
+INSERT INTO `tb_login` VALUES ('2', 'operator', '4b583376b2767b923c3e1da60d10de59', 'romy', 'operator');
 
-INSERT INTO `tb_login` (`kode_user`, `username`, `password`, `nama_lengkap`, `level`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Febriansyah Tri Romdhoni', 'admin'),
-(2, 'operator', '4b583376b2767b923c3e1da60d10de59', 'romy', 'operator');
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `id_pegawai` varchar(100) DEFAULT NULL,
+  `level` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+INSERT INTO `tb_user` VALUES ('1', 'okki', '0cc175b9c0f1b6a831c399e269772661', '1', '1');
+INSERT INTO `tb_user` VALUES ('2', 'romy', '0cc175b9c0f1b6a831c399e269772661', '2', '2');
 
---
--- Struktur dari tabel `usr`
---
-
+-- ----------------------------
+-- Table structure for usr
+-- ----------------------------
+DROP TABLE IF EXISTS `usr`;
 CREATE TABLE `usr` (
-  `kode_userr` int(11) NOT NULL,
+  `kode_userr` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(40) NOT NULL,
+  PRIMARY KEY (`kode_userr`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `usr`
---
-
-INSERT INTO `usr` (`kode_userr`, `username`, `password`) VALUES
-(1, 'asdafd', '74b87337454200d4d33f80c4663dc5e5');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_anggota`
---
-ALTER TABLE `tb_anggota`
-  ADD PRIMARY KEY (`kode_anggota`);
-
---
--- Indexes for table `tb_login`
---
-ALTER TABLE `tb_login`
-  ADD PRIMARY KEY (`kode_user`);
-
---
--- Indexes for table `usr`
---
-ALTER TABLE `usr`
-  ADD PRIMARY KEY (`kode_userr`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tb_login`
---
-ALTER TABLE `tb_login`
-  MODIFY `kode_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `usr`
---
-ALTER TABLE `usr`
-  MODIFY `kode_userr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of usr
+-- ----------------------------
+INSERT INTO `usr` VALUES ('1', 'asdafd', '74b87337454200d4d33f80c4663dc5e5');
